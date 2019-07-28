40cff7a4681722a874b4eb713014e3829440c2f6
estelamaga <estelinha.mag@hotmail.com>
2019-07-28 17:22:24 -0300

    Conectando mongodb com mongoose

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+//Conexão com o banco de dados
+
+const url = 'mongodb+srv://admin_user:doninha100db@clusterapi-56xut.mongodb.net/test?retryWrites=true&w=majority'
+const options = { reconnectTries: Number.MAX_VALUE, reconnetInterval: 500, poolSize: 5, useNewUrlJParser: true };
+
+mongoose.connect(url, options);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('error', (err) => {
+    console.log('erro na conexão com o banco de dados: ' + err);
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Aplicação desconectada do banco de dados');
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('Aplicação conectada ao banco');
+})
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const indexRoute = require('./Routes/index');
 const usersRoute = require('./Routes/users');
