293b88481bfb0bdc7c31cdf6501b3193b9b9d049
estelamaga <estelinha.mag@hotmail.com>
2019-08-02 14:23:38 -0300

    Retornando status code apropriados

--- Routes/users.js
+++ Routes/users.js
@@ -25,7 +25,7 @@ const createUserToken = (userId) =>{
         return res.send(users);
     }
     catch (err) {
-        return res.send({ error: 'Erro na na consulta do usuário'});
+        return res.status(500).send({ error: 'Erro na na consulta do usuário'});
     }
  });
 
@@ -52,17 +52,17 @@ const createUserToken = (userId) =>{
 router.post('/create', async (req, res) => {
     const { email, password } = req.body;
     
-    if (!email || !password) return res.send({ error: 'Dados insuficientes!'});
+    if (!email || !password) return res.status(400).send({ error: 'Dados insuficientes!'});
         
     try {
-        if (await Users.findOne({ email })) return res.send( { error: 'Usuário já cadastrado!'});
+        if (await Users.findOne({ email })) return res.status(400).send( { error: 'Usuário já cadastrado!'});
                 
         const user = await Users.create(req.body);
         user.password = undefined;
-        return res.send({user, token:createUserToken(user.id)});
+        return res.status(201).send({user, token:createUserToken(user.id)});
 
     } catch (err) {
-         return res.send({ error: 'Erro ao buscar usuário!'}); 
+         return res.status(500).send({ error: 'Erro ao buscar usuário!'}); 
     }
 });
  
@@ -92,20 +92,20 @@ router.post('/create', async (req, res) => {
 router.post('/auth', async (req, res) => {
     const { email, password } = req.body;
     
-    if (!email || !password) return res.send({ error: 'Dados insuficientes!'});
+    if (!email || !password) return res.status(400).send({ error: 'Dados insuficientes!'});
 
     try {
        const user = await Users.findOne({ email }).select('+password');
-       if (!user) return res.send( { error: 'Usuário não registrado'});
+       if (!user) return res.status(400).send( { error: 'Usuário não registrado'});
 
        const pass_ok =  await bcrypt.compare(password, user.password);
-       if (!pass_ok) return res.send({ error: 'Erro ao autenticar usuário!'});
+       if (!pass_ok) return res.status(401).send({ error: 'Erro ao autenticar usuário!'});
 
        user.password = undefined;
        return res.send({user, token:createUserToken(user.id)});
 
     } catch (err) {
-        return res.send({ error: 'Erro ao buscar usuário!'});
+        return res.status(500).send({ error: 'Erro ao buscar usuário!'});
 
     }
 
